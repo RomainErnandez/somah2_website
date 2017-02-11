0ae609dcf73f48b18f66b7e62e1263bf6f6a3493
Romain Ernandez <romain.ernandez@gmail.com>
2017-02-12 00:33:35 +0100

    {% block ul_nav %} in all templates

--- home/urls.py
+++ home/urls.py
@@ -4,9 +4,12 @@
 __author__ = 'rernande'
 
 from django.conf.urls import url
-from .views import dashboard, view_period
+from .views import dashboard, view_period, user, table, notifications
 
 urlpatterns = [
     url(r'^$', dashboard, name = 'dashboard'),
     url(r'^period/(\d+)/$', view_period, name = 'view_period'),  # Vue d'un
+    url(r'^user/$', user, name = 'user'),
+    url(r'^notifications/$', notifications, name = 'notifications'),
+    url(r'^table/$', table, name = 'table'),
 ]
--- home/views.py
+++ home/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render
 # Create your views here.
 from home.models import Period
 
-
 def dashboard(request):
     return render(request, template_name='home/dashboard.html')
 
@@ -16,3 +15,12 @@ def view_period(request, id_period):
         raise Http404
     else:
         return HttpResponse("period: " + str(period.id))
+
+def user(request):
+    return render(request, template_name='home/user.html')
+
+def notifications(request):
+    return render(request, template_name='home/notifications.html')
+
+def table(request):
+    return render(request, template_name='home/table.html')
