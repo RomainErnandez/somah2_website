1623c3804bf6f3818fdd38ec6c4fca392e099b72
Romain Ernandez <romain.ernandez@gmail.com>
2017-03-18 12:42:28 +0100

    Update settings.py

--- somah2_website/settings.py
+++ somah2_website/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static_root/'
+STATIC_URL = '/static/'
 
 ADMINS = (
     ('Romain Ernandez ', 'romain.ernandez@gmail.com'),
