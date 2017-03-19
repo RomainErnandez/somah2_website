2415bb06a56c128a14f7e634c30cd42bc553a0a2
Romain Ernandez <romain.ernandez@gmail.com>
2017-03-19 22:49:35 +0100

    settings for ci

--- somah2_website/settings.py
+++ somah2_website/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -138,7 +138,7 @@ MEDIA_URL = 'media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static_files'),)
+STATICFILES_DIRS = ()
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 # Simplified static file serving.
